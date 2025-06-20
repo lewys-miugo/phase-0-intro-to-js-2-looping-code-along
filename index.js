98c21f3a59098680475510352068120db2fb91bd
lewys-miugo <lewiswambugu01@gmail.com>
2025-06-20 14:57:24 +0300

    Writing functions to handle the tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  // console.log(messages);
+  return messages;
+}
+
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+  let i =0;
+  while(i<=number){
+    console.log(number - i);
+    i++;
+  }
+}
+
